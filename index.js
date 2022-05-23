a2d9f20e23eda857050a7d4177612b29ef17acff
Steven <stevendawn98@gmail.com>
2022-05-22 20:52:10 -0400

    commit tree a08f27ea03d349ef59b3b8ad7754ee76be2067b1

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
 }
+
+function deleteFromEmployeeByKey(employee, key) {
+    const deletedEmployee = {...employee}
+    delete deletedEmployee[key]
+    return deletedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   delete employee[key]
+   return employee;
+}
